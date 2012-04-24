cf5046de5cb2fa806d09cbbaae731dca67a6e586
Gabriel <extofer@gmail.com>
2012-04-23 21:20:54 -0600

    Added initializr CSS and JS libs

--- app/MUDBug/MUDBug/Properties/AssemblyInfo.cs
+++ app/MUDBug/MUDBug/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MUDBug")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Monitors Utility Databases")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("www.extofer.com")]
 [assembly: AssemblyProduct("MUDBug")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
